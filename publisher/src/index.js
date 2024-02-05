a12dfa65db1cfc8c3e70afb044e4e9cc1ec9caa9
balu <jotanlima@gmail.com>
2024-02-05 19:27:55 -0300

    refactor publisher

--- publisher/src/index.js
+++ publisher/src/index.js
@@ -1,1 +1,22 @@
 import 'dotenv/config'
+import amqplib from 'amqplib'
+
+import { rabbitmq } from './config.js'
+import data from './data.js'
+
+(async () => {
+  const queue = 'tasks'
+  const conn = await amqplib.connect(rabbitmq.url)
+
+  const ch1 = await conn.createChannel()
+  await ch1.assertQueue(queue)
+
+  const ch2 = await conn.createChannel()
+
+  data.map(msg => {
+    setInterval(() => {
+      ch2.sendToQueue(queue, Buffer.from(msg))
+      console.log(`Message sended: ${msg}`)
+    }, 1000)
+  })
+})()
