13962250aee11b81e9f8b67e3aa5624e0d43a273
balu <jotanlima@gmail.com>
2024-02-06 18:08:00 -0300

    improve publisher

--- publisher/src/config.js
+++ publisher/src/config.js
@@ -1,5 +1,6 @@
 export const rabbitmq = {
   url: process.env.RABBITMQ_URL || 'amqp://localhost',
-  queue: process.env.TASKS_QUEUE || 'tasks',
+  exchange: process.env.EXCHANGE || 'tasks',
+  routingKey: process.env.ROUTING_KEY || 'create',
   publisherSleep: Number(process.env.PUBLISHE_SLEEP_MS) || 1000
 }
