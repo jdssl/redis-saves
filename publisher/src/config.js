30ea1e052509e0a40040b360e9cd989b03e6c0d5
balu <jotanlima@gmail.com>
2024-02-06 18:45:19 -0300

    update publisher envs

--- publisher/src/config.js
+++ publisher/src/config.js
@@ -2,5 +2,5 @@ export const rabbitmq = {
   url: process.env.RABBITMQ_URL || 'amqp://localhost',
   exchange: process.env.EXCHANGE || 'tasks',
   routingKey: process.env.ROUTING_KEY || 'create',
-  publisherSleep: Number(process.env.PUBLISHE_SLEEP_MS) || 1000
+  publisherSleep: Number(process.env.PUBLISHER_SLEEP_MS) || 1000
 }
